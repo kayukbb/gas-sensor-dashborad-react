d8f2dbae7de2789c06ce95997d7a67f9740a9ed9
李家旭 <leekayuk@lijiaxudeMac-mini.local>
2024-09-05 23:57:23 +0800

    can show 60 records of graph properly

--- react/src/components/Charts/ChartOne.tsx
+++ react/src/components/Charts/ChartOne.tsx
@@ -122,7 +122,7 @@ const ChartOne: React.FC = () => {
   // Function to fetch data from the backend API
   const fetchData = async () => {
     try {
-      const amount = 60; // Limit to the last 60 records
+      const amount = 10; // Limit to the last 10 records
       const response = await api.get(`gasHistory/${amount}`); // Adjust the endpoint as necessary
 
       const readings = response.data; // Assuming this is the object with reading arrays
@@ -152,38 +152,13 @@ const ChartOne: React.FC = () => {
   useEffect(() => {
     // Fetch data initially
     fetchData();
-
-    // Set up an interval to fetch data every second
-    const intervalId = setInterval(() => {
-      fetchData(); // Fetch data on each interval
-    }, 5000); // 1000 ms = 1 second
-
-    // Cleanup function to clear the interval
-    return () => {
-      clearInterval(intervalId);
-    };
   }, []); // Empty dependency array means this effect runs once on mount
 
   return (
     <div className="col-span-12 rounded-sm border border-stroke bg-white px-5 pt-7.5 pb-5 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:col-span-8">
       <div className="flex flex-wrap items-start justify-between gap-3 sm:flex-nowrap">
-        <div className="flex w-full flex-wrap gap-3 sm:gap-5">
-          {state.series.map((s) => (
-            <div key={s.name} className="flex min-w-47.5">
-              <span className="mt-1 mr-2 flex h-4 w-full max-w-4 items-center justify-center rounded-full border border-primary">
-                <span className="block h-2.5 w-full max-w-2.5 rounded-full bg-primary"></span>
-              </span>
-              <div className="w-full">
-                <p className="font-semibold text-primary">{s.name}</p>
-                <p className="text-sm font-medium">
-                  Current Data: {s.data[s.data.length - 1] || 0}
-                </p>
-              </div>
-            </div>
-          ))}
-        </div>
+        <div className="flex w-full flex-wrap gap-3 sm:gap-5"></div>
       </div>
-
       <div>
         <div id="chartOne" className="-ml-5">
           <ReactApexChart
