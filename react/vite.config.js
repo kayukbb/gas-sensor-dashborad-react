9c15941d83c75350bca0ba4321b61f651cf8235f
李家旭 <leekayuk@lijiaxudeMac-mini.local>
2024-09-13 00:32:53 +0800

    test2

--- react/vite.config.js
+++ react/vite.config.js
@@ -5,4 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/gas-sensor-dashboard-react/',
+  server: {
+    port: 3001, // Change the port here
+  },
 });
