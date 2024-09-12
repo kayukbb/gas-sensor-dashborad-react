b0aaa88df298d7f56936cbd4831110fe0f8a0bb5
李家旭 <leekayuk@lijiaxudeMac-mini.local>
2024-09-12 22:49:46 +0800

    change base

--- react/vite.config.js
+++ react/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://kayukbb.github.io/gas-sensor-dashborad-react/react',
+  base: 'https://kayukbb.github.io/gas-sensor-dashborad-react/',
   server: {
     port: 3001, // Change the port here
   },
